48a223a9275a3e6d6a123be3204cfcbcb0d85ff9
prorochenko <33101252+prorochenko@users.noreply.github.com>
2023-01-12 14:44:56 +0200

    added from local

---
+++ src/components/HeroSection/Hero.js
@@ -0,0 +1,7 @@
+import css from './Hero.module.scss';
+
+const Hero = () => {
+  return <div className={css.box}></div>;
+};
+
+export default Hero;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import { store, persistor } from './redux/store';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import LoadingComponent from './components/Loader/Loader.jsx';
+import { HelmetProvider } from 'react-helmet-async';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { App } from './components/App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <Provider store={store}>
+      <PersistGate loading={<LoadingComponent />} persistor={persistor}> */}
+    <BrowserRouter basename="/mbox">
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </BrowserRouter>
+    {/* </PersistGate>
+    </Provider> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
